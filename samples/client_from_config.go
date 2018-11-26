84538b4118c4a3ccbed708d066a4af7964a39f9a
HikoQiu <hiko.qiu@qq.com>
2018-11-26 16:22:51 +0800

    fix sample urls

--- samples/client_from_config.go
+++ samples/client_from_config.go
@@ -12,7 +12,7 @@ func main() {
         "region-cn-hd-1": "zone-cn-hz-1",
     }
     config.ServiceUrl = map[string]string{
-        "zone-cn-hz-1": "http://192.168.20.236:9001/eureka",
+        "zone-cn-hz-1": "http://192.168.20.236:9001/eureka,http://192.168.20.237:9001/eureka",
     }
 
     // custom logger
